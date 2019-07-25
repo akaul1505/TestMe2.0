77637babd7917074dc1fb245f6b15b34db2c6013
akaul1505 <53161915+akaul1505@users.noreply.github.com>
2019-07-25 10:16:48 +0530

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -26,7 +26,7 @@ public class Stepdefs {
 	static int numberOfProducts; 
 	@Given("Alex is on the login page")
 	public void alex_is_on_the_login_page() {
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\abhishek.panwar\\Desktop\\Drivers\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\NexGen Testing Stream\DRIVERS\chromedriver_win32\chromedriver.exe");
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
